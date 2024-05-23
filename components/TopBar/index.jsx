4333143721e84996128a5c47e752d06384f3e565
Enkhtuvshin02 <159024942+Enkhtuvshin02@users.noreply.github.com>
2024-05-24 01:40:26 +0800

    Update index.jsx

--- components/TopBar/index.jsx
+++ components/TopBar/index.jsx
@@ -1,16 +1,20 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { API_BASE_URL } from "../../config.js";
 
 function Navbar({ loggedUsersName, handleLogout }) {
   return (
     <div>
       <ul className="nav justify-content-end">
-        <li className="nav-item">
-          <a className="nav-link" href="#">
-            {loggedUsersName}
-          </a>
-        </li>
+        {loggedUsersName && (
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              {loggedUsersName}
+            </a>
+          </li>
+        )}
+
         <li className="nav-item">
           <a
             className="nav-link "
